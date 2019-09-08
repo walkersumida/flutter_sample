6527f45bf41cb1332612fd6f00a8bd81de11eff5
WalkerSumida <walkersumida@gmail.com>
2019-09-08 13:29:28 +0900

    refact: fix indent

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Sample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Sample Login Page'),
+        title: 'Flutter Sample',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Sample Login Page'),
     );
   }
 }
@@ -57,84 +57,72 @@ class _MyHomePageState extends State<MyHomePage> {
       fetchAuth();
     }
     final emailField = TextField(
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-      onChanged: (value) {
-        setState(() {
-          _email = value;
-        });
-      },
+        obscureText: false,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Email",
+            border:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        onChanged: (value) {
+          setState(() {
+            _email = value;
+          });
+        },
     );
     final passwordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-      onChanged: (value) {
-        setState(() {
-          _password = value;
-        });
-      },
+        obscureText: true,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Password",
+            border:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        onChanged: (value) {
+          setState(() {
+            _password = value;
+          });
+        },
     );
     final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () { login(); },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color(0xff01A0C7),
+        child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () { login(); },
+            child: Text("Login",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
     );
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 130.0,
-                  child: Image.asset(
-                    "images/logo_flutter.png",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                SizedBox(height: 45.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(
-                  height: 35.0,
-                ),
-                loginButon,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+        body: Center(
+            child: Container(
+                color: Colors.white,
+                child: Padding(
+                    padding: const EdgeInsets.all(36.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(
+                              height: 80.0,
+                              child: Image.asset(
+                                  "images/logo_flutter.png",
+                                  fit: BoxFit.contain,
+                              ),
+                          ),
+                          SizedBox(height: 30.0),
+                          emailField,
+                          SizedBox(height: 10.0),
+                          passwordField,
+                          SizedBox(height: 35.0),
+                          loginButon,
+                          SizedBox(height: 15.0),
+                        ])))));
   }
 }
