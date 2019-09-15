bc533499daf058084bcf03fff18337e837377f97
WalkerSumida <walkersumida@gmail.com>
2019-09-15 22:50:44 +0900

    feat: add dashboard view

---
+++ lib/views/dashboard/index.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_sample/plugins/dio.dart';
+import 'package:flutter_sample/views/widgets/custom_dialog.dart';
+import 'package:flutter_sample/views/launch/index.dart';
+
+class ViewDashboardIndex extends StatefulWidget {
+  ViewDashboardIndex({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ViewDashboardIndexState createState() => _ViewDashboardIndexState();
+}
+
+class _ViewDashboardIndexState extends State<ViewDashboardIndex> {
+  CustomDialog dialog = new CustomDialog();
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+    fetchSignOut(context) async {
+      try {
+        final response =
+            await dio.delete('/auth/sign_out');
+        Navigator.pushAndRemoveUntil(
+            context,
+            new MaterialPageRoute(
+                builder: (context) => new ViewLaunchIndex()),
+            (_) => false);
+      } on DioError catch(e) {
+        print(e);
+      }
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Dashboad'),
+            leading: Builder(
+                builder: (BuildContext context) {
+                  return IconButton(
+                      icon: const Icon(Icons.menu),
+                      onPressed: () { Scaffold.of(context).openDrawer(); },
+                      tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                  );
+                },
+            ),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.white,
+            )),
+        drawer: Drawer(
+            child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  DrawerHeader(
+                      child: Text('Drawer Header'),
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                      ),
+                  ),
+                  ListTile(
+                      title: Text('Sign Out'),
+                      onTap: () {
+                        Navigator.pop(context);
+                        fetchSignOut(context);
+                      },
+                  ),
+                ]
+            )
+        ));
+  }
+}
