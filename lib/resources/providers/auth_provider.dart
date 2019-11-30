2619b99a76d4b279d12cac64f3d0877c378b7f16
WalkerSumida <walkersumida@gmail.com>
2019-11-30 16:08:43 +0900

    refact: create HandleError for provider class

--- lib/resources/providers/auth_provider.dart
+++ lib/resources/providers/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_sample/config/dio.dart';
 import 'package:flutter_sample/models/user_response.dart';
+import 'package:flutter_sample/resources/providers/handle_error.dart';
 
 class AuthProvider {
   Future<UserResponse> signIn(String email, String password) async {
@@ -11,39 +12,11 @@ class AuthProvider {
       return UserResponse.fromJson(response.data);
     } on DioError catch(error, stacktrace) {
       print("Exception occured: $error stackTrace: $stacktrace");
-      return UserResponse.withError(_handleError(error));
+      return UserResponse.withError(HandleError().errorDescription(error));
     }
   }
 
   Future<Response<dynamic>> signOut() async {
     return await dio.delete('/auth/sign_out');
-  }
-
-  String _handleError(DioError error) {
-    String errorDescription = "";
-    if (error is DioError) {
-      switch (error.type) {
-        case DioErrorType.CANCEL:
-          errorDescription = "Request to API server was cancelled";
-          break;
-        case DioErrorType.CONNECT_TIMEOUT:
-          errorDescription = "Connection timeout with API server";
-          break;
-        case DioErrorType.DEFAULT:
-          errorDescription =
-              "Connection to API server failed due to internet connection";
-          break;
-        case DioErrorType.RECEIVE_TIMEOUT:
-          errorDescription = "Receive timeout in connection with API server";
-          break;
-        case DioErrorType.RESPONSE:
-          errorDescription =
-              "Received invalid status code: ${error.response.statusCode}";
-          break;
-      }
-    } else {
-      errorDescription = "Unexpected error occured";
-    }
-    return errorDescription;
   }
 }
---
+++ lib/resources/providers/handle_error.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+
+class HandleError {
+  String errorDescription(DioError error) {
+    String errorDescription = "";
+    if (error is DioError) {
+      switch (error.type) {
+        case DioErrorType.CANCEL:
+          errorDescription = "Request to API server was cancelled";
+          break;
+        case DioErrorType.CONNECT_TIMEOUT:
+          errorDescription = "Connection timeout with API server";
+          break;
+        case DioErrorType.DEFAULT:
+          errorDescription =
+              "Connection to API server failed due to internet connection";
+          break;
+        case DioErrorType.RECEIVE_TIMEOUT:
+          errorDescription = "Receive timeout in connection with API server";
+          break;
+        case DioErrorType.RESPONSE:
+          errorDescription =
+              "Received invalid status code: ${error.response.statusCode}";
+          break;
+      }
+    } else {
+      errorDescription = "Unexpected error occured";
+    }
+    return errorDescription;
+  }
+}
